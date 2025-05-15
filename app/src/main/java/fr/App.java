7509e23a2b4bc27b03b8cba075d45b4de4740b02
Lucas Torri <58993481+SadPeanut@users.noreply.github.com>
2025-05-15 17:26:17 +0200

    Update App.java

--- app/src/main/java/fr/App.java
+++ app/src/main/java/fr/App.java
@@ -1,11 +1,18 @@
 package fr;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+
+        stage.show();
+        stage.toFront();
     }
 }
