fb880c7505d479c9841d5238bc30a43dd6c6a895
SadPeanut <slanistas29@gmail.com>
2025-05-17 20:02:21 +0200

    mvc

---
+++ app/src/main/java/fr/controller/SceneController.java
@@ -0,0 +1,14 @@
+package fr.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+
+public class SceneController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {}
+}
---
+++ app/src/main/java/fr/model/Blabla.java
@@ -0,0 +1,3 @@
+package fr.model;
+
+public class Blabla {}
